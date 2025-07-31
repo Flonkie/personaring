64efced79e9e5cf707fa575fd5c14c3887aab83d
Flonkie <147441082+Flonkie@users.noreply.github.com>
2025-07-31 21:58:05 +0100

    new members1

--- personawbr.js
+++ personawbr.js
@@ -1,6 +1,8 @@
 const personawbr = [
   {"host": "flonkie.nekoweb.org", "username": "flonkie"},
   {"host": "layercake.moe", "username": "June"},
+  {"host":"compendium.nekoweb.org","username":"Casio"},
+  {"host":"dreamscape.nekoweb.org","username":"Xiaoshan"},
 ];
 
 export default personawbr;
