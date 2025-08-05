e55ddb92efbccd0dc56153adc764f0964a568194
Flonkie <147441082+Flonkie@users.noreply.github.com>
2025-08-05 15:39:36 +0100

    update

--- personawbr.js
+++ personawbr.js
@@ -2,7 +2,7 @@ const personawbr = [
   {"host": "flonkie.nekoweb.org", "username": "flonkie"},
   {"host": "layercake.moe", "username": "June"},
   {"host":"compendium.nekoweb.org","username":"Casio"},
-  {"host":"dreamscape.nekoweb.org","username":"Xiaoshan"},
+  {"host":"dreamscape.place","username":"Xiaoshan"},
    {"host":"p4stelz.nekoweb.org","username":"Mary Jane"},
 ];
 
